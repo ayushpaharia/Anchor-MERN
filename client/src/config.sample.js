7b2a00d897aca56f907daf914d01637615cf1c28
ayushpaharia <ayush.paharia.18@gmail.com>
2020-09-16 01:38:17 +0530

    sample keys & config added

---
+++ client/src/config.sample.js
@@ -0,0 +1,11 @@
+export const pusher_id = "Your pusher id here";
+export const firebase_config = {
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId",
+};
